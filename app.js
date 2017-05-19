5aaf19b49cc90ff443ba705e96bb73126a6eab82
Pedro Felipe Elias de Souza <pedro.souza@micromed.net>
2017-05-19 16:55:57 -0300

    Modificacao

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-/* importar as configurações do servidor */
 var app = require('./config/server');
 
-/* parametrizar a porta de escuta */
 app.listen(38, function(){
 	console.log('Servidor online');
 })
